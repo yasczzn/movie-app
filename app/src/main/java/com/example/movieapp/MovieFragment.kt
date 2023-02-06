15443af882b4d0e588fab4d7eb665efd934a8ea9
yaswu <putriyasminarahmaz@gmail.com>
2023-02-06 13:54:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -0,0 +1,31 @@
+package com.example.movieapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MovieFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MovieFragment()
+    }
+
+    private lateinit var viewModel: MovieViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
